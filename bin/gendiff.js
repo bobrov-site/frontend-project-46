4771d7f813effb9ae8bc37d1261ec994a0a85ea4
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-10 18:41:19 +0300

    change gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,5 @@ const program = new Command()
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
-  .version('0.8.0');
+  .version('0.0.1')
 program.parse()
