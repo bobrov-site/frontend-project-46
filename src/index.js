e5adc8617a1526ca1199f6b606208d59446f9cd6
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-11 16:57:58 +0300

    add data1 and data2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import parseFile from "./parse"
-import { readFile } from "node:fs/promises"
+import parseFile from "./parse.js"
 
 const genDiff = (file1, file2) => {
-    console.log('hello')
+    const data1 = parseFile(file1)
+    const data2 = parseFile(file2)
+    console.log(data1, data2, 'data1 and data2')
 }
 
 export default genDiff
