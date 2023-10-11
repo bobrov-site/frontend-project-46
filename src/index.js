f3bb91bbe004caf40e1969f2c2bfed769b1ede7c
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-11 21:07:19 +0300

    add sortObject

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import parseFile from "./parse.js"
 
+const sortObject = (obj) => {
+    const newObj = {}
+    const keys = Object.keys(obj).sort()
+    for (const key of keys) {
+        newObj[key] = obj[key]
+    }
+    return newObj
+}
+
 const genDiff = (file1, file2) => {
     const data1 = parseFile(file1)
     const data2 = parseFile(file2)
-    console.log(data1, data2, 'data1 and data2')
+    const sortedData1 = sortObject(data1)
+    const sortedData2 = sortObject(data2)
+    console.log(sortedData1, sortedData2)
+    //has https://lodash.com/docs/4.17.15#has
+    //
+    
 }
 
 export default genDiff
