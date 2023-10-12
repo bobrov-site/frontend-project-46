c3ee27410cc3917b2315cd5385748a99dffc5981
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-12 17:26:41 +0300

    add function makeTree

--- src/index.js
+++ src/index.js
@@ -18,22 +18,38 @@ const compareData = (data1, data2) => {
         if (!_.has(data2, key)) {
             comparedData[key] = {status: 'deleted', value1: data1[key]}
         }
-        if (_.has(data2, key) && data1[key] !== data2[key]) {
+        if (!_.has(data1, key) && _.has(data2, key)) {
+            comparedData[key] = {status: 'added', value2: data2[key]}
+        }
+        if (_.has(data2, key) && data1[key] !== data2[key] && data1[key] !== undefined) {
             comparedData[key] = {status: 'updated', value1: data1[key], value2: data2[key]}
         }
         if (_.has(data2, key) && data1[key] === data2[key]) {
             comparedData[key] = {status: 'same', value1: data1[key]}
         }
     }
-    console.log(comparedData)
+    return comparedData
 }
 
 const makeTree = (comparedData) => {
+    console.log(comparedData)
     const keys = Object.keys(comparedData)
-    const string = ''
+    let tree = '{\n'
     for (const key of keys) {
-        
+        if (comparedData[key].status === 'deleted') {
+            tree += `- ${key}: ${comparedData[key].value1}\n`
+        }
+        if (comparedData[key].status === 'updated') {
+            tree += `- ${key}: ${comparedData[key].value1}\n`
+            tree += `+ ${key}: ${comparedData[key].value2}\n`
+        }
+        if (comparedData[key].status === 'added') {
+            tree += `+ ${key}: ${comparedData[key].value2}\n`
+        }   
     }
+    tree += '}'
+    console.log(tree)
+    return tree
 }
 
 const genDiff = (file1, file2) => {
@@ -43,7 +59,6 @@ const genDiff = (file1, file2) => {
     const sortedData2 = sortObject(data2)
     const comparedData = compareData(sortedData1, sortedData2)
     const tree = makeTree(comparedData)
-    console.log(comparedData)
 }
 
 export default genDiff
