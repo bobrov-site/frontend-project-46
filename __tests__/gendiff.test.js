6971ef5da93b9cc43623c6479016b0f11555ee78
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-16 10:31:06 +0300

    change expected in gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,15 +5,7 @@ import path from 'node:path';
 import { cwd } from 'node:process';
 
 test('shoud be equal to expected', () => {
-    const expected = `{
-        - host: hexlet.io,
-        + host: google.com,
-        - timeout: 50,
-        + timeout: 30
-          verbose: true,
-          proxy: 123.234.53.22,
-          follow: false
-    }`
+    const expected = readFileSync('__fixtures__/expectedFile.txt', 'utf8')
     const file1 = './__fixtures__/file1.json'
     const file2 = './__fixtures__/file2.json'
 
