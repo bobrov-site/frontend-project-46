11b87a8dff8abf0577d7cad2647c3b45517a5c8a
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-16 11:50:00 +0300

    make fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,12 @@
 import { readFileSync } from 'node:fs';
-import {test, expect} from '@jest/globals'
+import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
-import path from 'node:path';
-import { cwd } from 'node:process';
 
 test('shoud be equal to expected', () => {
-    const expected = readFileSync('__fixtures__/expectedFile.txt', 'utf8')
-    const file1 = './__fixtures__/file1.json'
-    const file2 = './__fixtures__/file2.json'
+  const expected = readFileSync('__fixtures__/expectedFile.txt', 'utf8');
+  const file1 = './__fixtures__/file1.json';
+  const file2 = './__fixtures__/file2.json';
 
-    const resived = genDiff(file1, file2)
-    expect(resived).toBe(expected)
-})
+  const resived = genDiff(file1, file2);
+  expect(resived).toBe(expected);
+});
