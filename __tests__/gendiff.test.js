1b1add58e0fd9616349614fe2f09f0a745bf1657
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-16 10:19:15 +0300

    add test in gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,22 @@
+import { readFileSync } from 'node:fs';
 import {test, expect} from '@jest/globals'
-import genDiff from '@/src/index.js';
-test('shoud be return value', () => {
-    expect(1).toBe(1)
+import genDiff from '../src/index.js';
+import path from 'node:path';
+import { cwd } from 'node:process';
+
+test('shoud be equal to expected', () => {
+    const expected = `{
+        - host: hexlet.io,
+        + host: google.com,
+        - timeout: 50,
+        + timeout: 30
+          verbose: true,
+          proxy: 123.234.53.22,
+          follow: false
+    }`
+    const file1 = './__fixtures__/file1.json'
+    const file2 = './__fixtures__/file2.json'
+
+    const resived = genDiff(file1, file2)
+    expect(resived).toBe(expected)
 })
