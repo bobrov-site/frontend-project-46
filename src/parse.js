42280ffc97cea4b29104bc08c5dcbc3427de89e7
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-16 18:29:36 +0300

    add .yaml case in parse.js

--- src/parse.js
+++ src/parse.js
@@ -13,6 +13,8 @@ const getFileParse = (data, fileType) => {
       return JSON.parse(data);
     case '.yml':
       return yaml.load(data);
+    case '.yaml':
+      return yaml.load(data)
     default:
       throw new Error(`Unknown extension: '${fileType}'!`);
   }
