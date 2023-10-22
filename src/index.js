77d7d07a5215fd6d5b42c68cf34de204cfe570cd
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-22 14:48:12 +0300

    change makeTree in index.js

--- src/index.js
+++ src/index.js
@@ -26,30 +26,31 @@ const compareData = (data1, data2) => {
     if (data1[key] === data2[key]) {
       return {name: key, status: 'same', value: data1[key] };
     }
-    return {
-      key, status: 'updated', value1: data1[key], value2: data2[key],
-    };
+    return {name: key, status: 'updated', value1: data1[key], value2: data2[key]};
   })
 };
 
-const addMargin = (marginCount, margin = ' ') => _.repeat(margin, marginCount);
+const addMargin = (marginCount, marginSymbol = ' ') => _.repeat(marginSymbol, marginCount);
 
-const makeTree = (comparedData) => {
+const makeTree = (comparedData, depth = 1) => {
+  // console.log(comparedData, 'data')
   const data = comparedData.map((item) => {
+    if (item.status === 'nested') {
+      return `${addMargin(depth * 4 - 0)}${item.name}: ${makeTree(item.children, depth + 1)}`;
+    }
     if (item.status === 'deleted') {
-      console.log(`${addMargin(2)}- ${item.name}: ${item.value1}`)
-      return `${addMargin(2)}- ${item.name}: ${item.value1}`;
+      return `${addMargin(depth * 4 - 2)}- ${item.name}: ${item.value1}`;
     }
     if (item.status === 'added') {
-      return `${addMargin(2)}+ ${item.name}: ${item.value2}`;
+      return `${addMargin(depth * 4 - 2)}+ ${item.name}: ${item.value2}`;
     }
     if (item.status === 'updated') {
-      const str1 = `${addMargin(2)}- ${item.name}: ${item.value1}`;
-      const str2 = `${addMargin(2)}+ ${item.name}: ${item.value2}`;
+      const str1 = `${addMargin(depth * 4 - 2)}- ${item.name}: ${item.value1}`;
+      const str2 = `${addMargin(depth * 4 - 2)}+ ${item.name}: ${item.value2}`;
       return `${str1}\n${str2}`;
     }
     if (item.status === 'same') {
-      return `${addMargin(4)}${item.name}: ${item.value1}`;
+      return `${addMargin(depth * 4 - 2)}${item.name}: ${item.value}`;
     }
     return item;
   });
