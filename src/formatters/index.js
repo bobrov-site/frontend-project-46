a13610ade5d06d9ce87a2203eca10124f91e9274
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-23 13:48:01 +0300

    add switch to formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,13 @@
 import setStylish from "./stylish.js"
 
 const setFormatter = (data, format) => {
-    if (format === 'stylish') {
-        return setStylish(data)
+    switch (format) {
+        case 'stylish':
+            return setStylish(data)
+        default:
+            throw new Error(`Unknown format: '${format}'!`);
     }
+    
 }
 
 export default setFormatter
