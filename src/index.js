c11bb9d08a4e0de4a7872f471b27b2dd3f129168
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-23 13:25:36 +0300

    replaced addMargin, getString, makeTree from index.js to stylish.js

--- src/index.js
+++ src/index.js
@@ -32,48 +32,14 @@ const compareData = (data1, data2) => {
   })
 };
 
-const addMargin = (marginCount, marginSymbol = ' ') => _.repeat(marginSymbol, marginCount);
-
-const getString = (data, depth) => {
-  if (typeof data !== 'object' || data === null) {
-    return `${data}`
-  }
-  const str = Object.entries(data).map(([key, value]) => `${addMargin(depth * 4 - (-3))} ${key}: ${getString(value, depth + 1)}`)
-  return `{\n${str.join('\n')}\n${addMargin(depth * 4 - 0)}}`
-}
-
-const makeTree = (comparedData, depth = 1) => {
-  const data = comparedData.map((item) => {
-    if (item.status === 'nested') {
-      return `${addMargin(depth * 4 - 0)}${item.name}: ${makeTree(item.children, depth + 1)}`;
-    }
-    if (item.status === 'deleted') {
-      return `${addMargin(depth * 4 - 2)}- ${item.name}: ${getString(item.value1, depth)}`;
-    }
-    if (item.status === 'added') {
-      return `${addMargin(depth * 4 - 2)}+ ${item.name}: ${getString(item.value2, depth)}`;
-    }
-    if (item.status === 'updated') {
-      const str1 = `${addMargin(depth * 4 - 2)}- ${item.name}: ${getString(item.value1, depth)}`;
-      const str2 = `${addMargin(depth * 4 - 2)}+ ${item.name}: ${getString(item.value2, depth)}`;
-      return `${str1}\n${str2}`;
-    }
-    if (item.status === 'same') {
-      return `${addMargin(depth * 4 - 0)}${item.name}: ${getString(item.value)}`;
-    }
-    return item;
-  });
-  return `{\n${data.join('\n')}\n${addMargin(depth * 4 - 4)}}`;
-};
-
 const genDiff = (file1, file2, formatter = 'stylish') => {
   const data1 = parseFile(file1);
   const data2 = parseFile(file2);
   const sortedData1 = sortData(data1);
   const sortedData2 = sortData(data2);
   const comparedData = compareData(sortedData1, sortedData2);
-  const tree = makeTree(comparedData);
-  return tree;
+  const formattedData = setFormatter(comparedData, formatter)
+  return formattedData
 };
 
 export default genDiff;
