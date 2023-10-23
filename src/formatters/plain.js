fccd7e4ac3556f2d97d6b0458900525387dd2134
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-23 15:58:41 +0300

    add makePlain in plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,3 +1,7 @@
-const setPlain = (data) => makeTree(data)
+const makePlain = (comparedData, depth = 1) => {
+
+}
+
+const setPlain = (comparedData) => makeTree(comparedData)
 
 export default setPlain
