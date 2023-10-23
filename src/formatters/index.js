70ee8d19200464abfe0d6e440fa3877e2f72e738
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-23 16:03:33 +0300

    add data as argument in formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,12 @@
 import setStylish from './stylish.js';
+import setPlain from './plain.js';
 
 const setFormatter = (data, format) => {
   switch (format) {
     case 'stylish':
       return setStylish(data);
     case 'plain':
-      return setPlain
+      return setPlain(data)
     default:
       throw new Error(`Unknown format: '${format}'!`);
   }
