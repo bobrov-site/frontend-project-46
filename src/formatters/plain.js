a57a67b63085f0f599b99f5cf4854895914bb860
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-23 16:14:31 +0300

    add getString to plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,5 +1,17 @@
-const makePlain = (comparedData, depth = 1) => {
+const getString = (data) => {
+    if (typeof data !== 'object' || data === null) {
 
+    }
+    return `[complex value]`
+}
+
+const makePlain = (comparedData, depth = 1) => {
+    const data = comparedData.map((item) => {
+        if (item.status === 'added') {
+            return `Property '${item.name}' was added with value: ${getString(item.value2)}`
+        }
+    })
+    return `${data.join('\n')}`
 }
 
 const setPlain = (comparedData) => makePlain(comparedData)
