1c60bdde2b6158e3168da4a38da51f175b7c0318
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-24 16:07:47 +0300

    remove sort() from index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import setFormatter from './formatters/index.js';
 // rename to Data
 const sortData = (obj) => {
   const sorted = {};
-  Object.keys(obj).sort().forEach((key) => {
+  _.sortBy(Object.keys(obj)).forEach((key) => {
     sorted[key] = obj[key];
   });
+  console.log(sorted)
   return sorted;
 };
 const compareData = (data1, data2) => {
