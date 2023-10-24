217fbad9db4660197879d81d5aa19a58bb9a356d
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-24 14:21:55 +0300

    add json jormatter in formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,6 @@
 import setStylish from './stylish.js';
 import setPlain from './plain.js';
+import setJson from './json.js';
 
 const setFormatter = (data, format) => {
   switch (format) {
@@ -7,6 +8,8 @@ const setFormatter = (data, format) => {
       return setStylish(data);
     case 'plain':
       return setPlain(data);
+    case 'json':
+      return setJson(data);
     default:
       throw new Error(`Unknown format: '${format}'!`);
   }
