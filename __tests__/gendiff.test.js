8ad47c454125e5115a5c7d213c45c9c633ce98fc
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-24 14:03:32 +0300

    add test for yaml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -60,4 +60,32 @@ describe('gendiff plain', () => {
     const resived = genDiff(file1, file2, 'plain');
     expect(resived).toBe(expected);
   });
+  test('shoud be equal string data from .yml file', () => {
+    const expected = readFile('expectedFileStylish.txt');
+
+    const file1 = getFixturePath('file1.yml');
+    const file2 = getFixturePath('file2.yml');
+
+    const resived = genDiff(file1, file2, 'stylish');
+    expect(resived).toBe(expected);
+  });
+  test('shoud be equal string data from .yaml file', () => {
+    const expected = readFile('expectedFileStylish.txt');
+
+    const file1 = getFixturePath('file1.yaml');
+    const file2 = getFixturePath('file2.yaml');
+
+    const resived = genDiff(file1, file2, 'stylish');
+    expect(resived).toBe(expected);
+  });
+
+  test('shoud be equal file1.json and file2.yml to string data', () => {
+    const expected = readFile('expectedFileStylish.txt');
+
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.yaml');
+
+    const resived = genDiff(file1, file2);
+    expect(resived).toBe(expected);
+  });
 });
