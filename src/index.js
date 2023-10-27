03ce6315678e83a1457b27b1266609cfd951f0bb
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-27 11:44:50 +0300

    rename functions in index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import parseFile from './parse.js';
 import compareData from './compare.js';
 import setFormatter from './formatters/index.js';
 
-const getAbsoluteFilePath = (file) => path.resolve(cwd(), file);
+const buildFullPath = (file) => path.resolve(cwd(), file);
 
-const getFileType = (file) => path.extname(file).replace('.', '');
+const extractFileType = (file) => path.extname(file).replace('.', '');
 
 const readFile = (file) => {
-  const filePath = getAbsoluteFilePath(file);
+  const filePath = buildFullPath(file);
   const fileData = readFileSync(filePath, 'utf8');
   return fileData;
 };
 
 const genDiff = (file1, file2, formatter = 'stylish') => {
-  const fileType1 = getFileType(file1);
-  const fileType2 = getFileType(file2);
+  const fileType1 = extractFileType(file1);
+  const fileType2 = extractFileType(file2);
   const fileData1 = readFile(file1);
   const fileData2 = readFile(file2);
   const data1 = parseFile(fileData1, fileType1);
