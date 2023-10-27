757b56cbcee9ffb44dfa6301ce77d0f651e2fd22
Daniil Bobrov <allodrmen@yandex.ru>
2023-10-27 11:22:56 +0300

    make fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { readFileSync } from 'node:fs';
+import path from 'node:path';
+import { cwd } from 'node:process';
 import parseFile from './parse.js';
 import compareData from './compare.js';
 import setFormatter from './formatters/index.js';
-import path from 'node:path';
-import { cwd } from 'node:process';
 
 const getFilePath = (file) => path.resolve(cwd(), file);
 
@@ -12,14 +12,14 @@ const getFileType = (file) => path.extname(file);
 const readFile = (file) => {
   const filePath = getFilePath(file);
   const fileData = readFileSync(filePath, 'utf8');
-  return fileData
-}
+  return fileData;
+};
 
 const genDiff = (file1, file2, formatter = 'stylish') => {
-  const fileType1 = getFileType(file1)
-  const fileType2 = getFileType(file2)
-  const fileData1 = readFile(file1)
-  const fileData2 = readFile(file2)
+  const fileType1 = getFileType(file1);
+  const fileType2 = getFileType(file2);
+  const fileData1 = readFile(file1);
+  const fileData2 = readFile(file2);
   const data1 = parseFile(fileData1, fileType1);
   const data2 = parseFile(fileData2, fileType2);
   const comparedData = compareData(data1, data2);
