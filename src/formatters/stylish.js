9efd6158f30d70e49d8d8dbef911ce5e37925144
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-02 18:03:26 +0300

    refactoring margin in stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,37 +1,42 @@
 import _ from 'lodash';
 
-const addMargin = (marginCount, marginSymbol = ' ') => _.repeat(marginSymbol, marginCount);
+const addMargin = (depth, fullMargin = false, lol = false) => { 
+  if (fullMargin) {
+    return _.repeat(' ', depth * 4 - 2)
+  }
+  return _.repeat(' ', depth * 4 - 0);
+}
 
 const makeString = (item, depth) => {
   if (!_.isObject(item) || item === null) {
     return `${item}`;
   }
-  const str = Object.entries(item).map(([key, value]) => `${addMargin(depth * 4 - (-3))} ${key}: ${makeString(value, depth + 1)}`);
-  return `{\n${str.join('\n')}\n${addMargin(depth * 4 - 0)}}`;
+  const str = Object.entries(item).map(([key, value]) => ` ${addMargin(depth + 1, true)} ${key}: ${makeString(value, depth + 1)}`);
+  return `{\n${str.join('\n')}\n${addMargin(depth)}}`;
 };
 
 const makeTree = (comparedData, depth = 1) => {
   const data = comparedData.map((item) => {
     if (item.type === 'nested') {
-      return `${addMargin(depth * 4 - 0)}${item.key}: ${makeTree(item.children, depth + 1)}`;
+      return `${addMargin(depth)}${item.key}: ${makeTree(item.children, depth + 1)}`;
     }
     if (item.type === 'deleted') {
-      return `${addMargin(depth * 4 - 2)}- ${item.key}: ${makeString(item.value1, depth)}`;
+      return `${addMargin(depth, true)}- ${item.key}: ${makeString(item.value1, depth)}`;
     }
     if (item.type === 'added') {
-      return `${addMargin(depth * 4 - 2)}+ ${item.key}: ${makeString(item.value2, depth)}`;
+      return `${addMargin(depth, true)}+ ${item.key}: ${makeString(item.value2, depth)}`;
     }
     if (item.type === 'updated') {
-      const str1 = `${addMargin(depth * 4 - 2)}- ${item.key}: ${makeString(item.value1, depth)}`;
-      const str2 = `${addMargin(depth * 4 - 2)}+ ${item.key}: ${makeString(item.value2, depth)}`;
+      const str1 = `${addMargin(depth, true)}- ${item.key}: ${makeString(item.value1, depth)}`;
+      const str2 = `${addMargin(depth, true)}+ ${item.key}: ${makeString(item.value2, depth)}`;
       return `${str1}\n${str2}`;
     }
     if (item.type === 'same') {
-      return `${addMargin(depth * 4 - 0)}${item.key}: ${makeString(item.value)}`;
+      return `${addMargin(depth)}${item.key}: ${makeString(item.value)}`;
     }
     return item;
   });
-  return `{\n${data.join('\n')}\n${addMargin(depth * 4 - 4)}}`;
+  return `{\n${data.join('\n')}\n${addMargin(depth - 1)}}`;
 };
 
 const makeStylishReportDiff = (comparedData) => makeTree(comparedData);
