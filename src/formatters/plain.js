db8f64a0009f82aa9f8a2a11c0c103f1adffdcec
Daniil Bobrov <allodrmen@yandex.ru>
2023-11-03 09:02:51 +0300

    add one line returing comparedData in plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,24 +10,25 @@ const makeString = (item) => {
   return String(item)
 };
 const makePlain = (comparedData, path = '') => {
-  const filtredData = comparedData.filter((item) => item.type !== 'same');
-  const data = filtredData.map((item) => {
-    const itemPath = `${path}${item.key}`;
-    if (item.type === 'nested') {
-      return makePlain(item.children, `${itemPath}.`);
-    }
-    if (item.type === 'added') {
-      return `Property '${itemPath}' was added with value: ${makeString(item.value2)}`;
-    }
-    if (item.type === 'deleted') {
-      return `Property '${itemPath}' was removed`;
-    }
-    if (item.type === 'updated') {
-      return `Property '${itemPath}' was updated. From ${makeString(item.value1)} to ${makeString(item.value2)}`;
-    }
-    throw new Error(`Unknown type: '${item.type}'!`);
-  });
-  return data.join('\n');
+  return comparedData
+    .filter((item) => item.type !== 'same')
+    .map((item) => {
+      const itemPath = `${path}${item.key}`;
+      if (item.type === 'nested') {
+        return makePlain(item.children, `${itemPath}.`);
+      }
+      if (item.type === 'added') {
+        return `Property '${itemPath}' was added with value: ${makeString(item.value2)}`;
+      }
+      if (item.type === 'deleted') {
+        return `Property '${itemPath}' was removed`;
+      }
+      if (item.type === 'updated') {
+        return `Property '${itemPath}' was updated. From ${makeString(item.value1)} to ${makeString(item.value2)}`;
+      }
+      throw new Error(`Unknown type: '${item.type}'!`);
+    })
+    .join('\n');
 };
 
 const makePlainReportDiff = (comparedData) => makePlain(comparedData);
